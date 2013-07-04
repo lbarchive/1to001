3a4b23dcd8f11700c2ffdbf12f4f7300ee6584d2
Yu-Jie Lin <livibetter@gmail.com>
2013-07-04 21:44:36 +0800

    add tests

--- 1to001.py
+++ 1to001.py
@@ -6,11 +6,12 @@ import sys
 RE = re.compile('([0-9]+|[^0-9]+)')
 
 
-def main():
+class Error(Exception):
 
-  ofns = sys.argv[1:]
-  if not ofns:
-    return
+  pass
+
+
+def get_cfns(ofns):
 
   fns = [RE.findall(fn) for fn in ofns]
   fn0 = fns[0]
@@ -18,8 +19,7 @@ def main():
 
   # same number of fields
   if any(nfields != len(fn) for fn in fns):
-    print('different number of fields', file=sys.stderr)
-    return
+    raise Error(1, 'different number of fields')
 
   # find fields contain numbers
   n_idx = [idx for idx, item in enumerate(fn0)
@@ -29,9 +29,7 @@ def main():
   if any(fn0[idx] != fn[idx]
          for fn in fns
          for idx in range(nfields) if idx not in n_idx):
-    print('at least one non-number field does not have same content',
-          file=sys.stderr)
-    return
+    raise Error(2, 'at least one non-number field does not have same content')
 
   # find the maximal length
   n = [(idx, max(len(fn[idx]) for fn in fns))
@@ -39,31 +37,53 @@ def main():
 
   # pad zeros
   cfns = []
+  dfns = []
   for fn, ofn in zip(fns, ofns):
     dfn = fn[:]
     for idx, maxlen in n:
       l = maxlen - len(fn[idx])
       if not l:
         continue
-      fn[idx] = '0'*l + fn[idx]
-      dfn[idx] = '\033[1;32m0\033[0m'*l + dfn[idx]
+      fn[idx] = '0' * l + fn[idx]
+      dfn[idx] = '\033[1;32m0\033[0m' * l + dfn[idx]
 
     nfn = ''.join(fn)
     if nfn != ofn:
       cfns.append((ofn, nfn))
-      print(''.join(dfn))
+      dfns.append(''.join(dfn))
+
+  return cfns, dfns
+
+
+def do_renaming(cfns):
+
+  for ofn, nfn in cfns:
+    print('%s -> %s' % (ofn, nfn))
+    os.rename(ofn, nfn)
+
+
+def main():
+
+  fns = sys.argv[1:]
+  if not fns:
+    return
+
+  try:
+    cfns, dfns = get_cfns(fns)
+  except Error as e:
+    print('Error %d: %s' % e.args, file=sys.stderr)
+    sys.exit(1)
 
   if not cfns:
     print('nothing to pad zeros')
     return
 
-  if input('perform padding (y/n)? ') != 'y':
-    return
+  for dfn in dfns:
+    print(dfn)
 
-  # do renaming
-  for ofn, nfn in cfns:
-    print('%s -> %s' % (ofn, nfn))
-    os.rename(ofn, nfn)
+  if input('perform padding (y/n)? ') == 'y':
+    do_renaming(cfns)
+
 
 if __name__ == '__main__':
   main()
---
+++ tests.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+import imp
+import unittest
+
+m = imp.load_source('1to001', '1to001.py')
+
+
+class TestFile(unittest.TestCase):
+
+  def test_no_renames(self):
+
+    fns = [
+      '1.txt',
+      '2.txt',
+    ]
+    expect = []
+
+    cfns, dfns = m.get_cfns(fns)
+    self.assertEqual(cfns, expect)
+
+  def test_simple(self):
+
+    fns = [
+      '1.txt',
+      '10.txt',
+    ]
+    expect = [
+      ('1.txt', '01.txt'),
+    ]
+
+    cfns, dfns = m.get_cfns(fns)
+    self.assertEqual(cfns, expect)
+
+  def test_leading_text(self):
+
+    fns = [
+      'a1.txt',
+      'a10.txt',
+    ]
+    expect = [
+      ('a1.txt', 'a01.txt'),
+    ]
+
+    cfns, dfns = m.get_cfns(fns)
+    self.assertEqual(cfns, expect)
+
+  def test_tailing_text(self):
+
+    fns = [
+      '1b.txt',
+      '10b.txt',
+    ]
+    expect = [
+      ('1b.txt', '01b.txt'),
+    ]
+
+    cfns, dfns = m.get_cfns(fns)
+    self.assertEqual(cfns, expect)
+
+  def test_surrounding_texts(self):
+
+    fns = [
+      'a1b.txt',
+      'a10b.txt',
+    ]
+    expect = [
+      ('a1b.txt', 'a01b.txt'),
+    ]
+
+    cfns, dfns = m.get_cfns(fns)
+    self.assertEqual(cfns, expect)
+
+  def test_more_texts(self):
+
+    fns = [
+      'something a1b.txt',
+      'something a10b.txt',
+    ]
+    expect = [
+      ('something a1b.txt', 'something a01b.txt'),
+    ]
+
+    cfns, dfns = m.get_cfns(fns)
+    self.assertEqual(cfns, expect)
+
+  def test_2_number_fields(self):
+
+    fns = [
+      'a1b 33.txt',
+      'a10b 3.txt',
+    ]
+    expect = [
+      ('a1b 33.txt', 'a01b 33.txt'),
+      ('a10b 3.txt', 'a10b 03.txt'),
+    ]
+
+    cfns, dfns = m.get_cfns(fns)
+    self.assertEqual(cfns, expect)
+
+  ##############
+  # Exceptions #
+  ##############
+
+  def test_different_fields_error(self):
+
+    fns = [
+      '1.txt',
+      '1 0.txt',
+    ]
+
+    with self.assertRaises(m.Error) as e:
+      cfns, dfns = m.get_cfns(fns)
+
+    self.assertEqual(e.exception.args[0], 1)
+
+  def test_content_differ_error(self):
+
+    fns = [
+      '1 a.txt',
+      '1 b.txt',
+    ]
+
+    with self.assertRaises(m.Error) as e:
+      cfns, dfns = m.get_cfns(fns)
+
+    self.assertEqual(e.exception.args[0], 2)
+
+
+if __name__ == '__main__':
+
+  unittest.main()
